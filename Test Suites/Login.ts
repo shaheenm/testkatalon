1c90b342f9e67d4efa3bf09da83b204640a3cd60
Shaheen Mahani <shaheenm@advisorwebsites.com>
2017-06-27 16:03:24 -0700

    TestCase/suite

---
+++ Test Suites/Login.ts
@@ -0,0 +1,38 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Login</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <lastRun>2017-06-27T15:33:33</lastRun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>35e019b7-6f78-457c-adee-2edbf1e36647</testSuiteGuid>
+   <testCaseLink>
+      <guid>3c9ba414-7bad-4408-b03a-da59fdefa594</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Login - Wrong username</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>2cbc260a-cf8c-4572-8a6d-3f1ba829b6f7</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Login - Wrong Password</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>b6305410-99b8-44b8-82a9-0d4368e23f3a</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Login - Forgot Password wrong email</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>eeb4472c-879c-443d-bc58-f136a759dd7a</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Login - Forgot Password correct email</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
